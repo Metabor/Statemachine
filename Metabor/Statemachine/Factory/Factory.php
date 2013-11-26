ad4ab3fb88990e3d164bc2544485adc6d841a353
Oliver Tischlinger <oliver.tischlinger@project-a.com>
2013-11-26 18:50:17 +0100

    register observer instances on factory for created statemachines

--- Metabor/Statemachine/Factory/Factory.php
+++ Metabor/Statemachine/Factory/Factory.php
@@ -24,6 +24,11 @@ class Factory implements FactoryInterface
     private $stateNameDetector;
 
     /**
+     * @var \SplObjectStorage
+     */
+    private $statemachineObserver;
+
+    /**
      * @param ProcessDetectorInterface $processDetector
      * @param StateNameDetectorInterface $stateNameDetector
      */
@@ -32,6 +37,31 @@ class Factory implements FactoryInterface
     {
         $this->processDetector = $processDetector;
         $this->stateNameDetector = $stateNameDetector;
+        $this->statemachineObserver = new \SplObjectStorage();
+    }
+
+    /**
+     * @param \SplObserver $observer
+     */
+    public function attachStatemachineObserver(\SplObserver $observer)
+    {
+        $this->statemachineObserver->attach($observer);
+    }
+
+    /**
+     * @param \SplObserver $observer
+     */
+    public function detachStatemachineObserver(\SplObserver $observer)
+    {
+        $this->statemachineObserver->detach($observer);
+    }
+
+    /**
+     * @return \Traversable
+     */
+    public function getStatemachineObserver()
+    {
+        return $this->statemachineObserver;
     }
 
     /**
@@ -44,9 +74,15 @@ class Factory implements FactoryInterface
         $process = $this->processDetector->detectProcess($subject);
         if ($this->stateNameDetector) {
             $stateName = $this->stateNameDetector->detectCurrentStateName($subject);
-            return new Statemachine($subject, $process, $stateName);
+            $statemachine = new Statemachine($subject, $process, $stateName);
         } else {
-            return new Statemachine($subject, $process);
+            $statemachine = new Statemachine($subject, $process);
         }
+
+        foreach ($this->statemachineObserver as $observer) {
+            $statemachine->attach($observer);
+        }
+
+        return $statemachine;
     }
 }
