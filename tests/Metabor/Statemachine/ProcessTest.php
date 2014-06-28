961321886bfacd6f4bb32fbd1e0328abee11a5ca
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-06-28 22:22:07 +0200

    update composerAdd UnitTest

---
+++ tests/Metabor/Statemachine/ProcessTest.php
@@ -0,0 +1,30 @@
+<?php
+namespace Metabor\Statemachine;
+use MetaborStd\Statemachine\ProcessInterfaceTest;
+
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class ProcessTest extends ProcessInterfaceTest
+{
+    /**
+     * 
+     * @see \MetaborStd\NamedInterfaceTest::createTestInstance()
+     */
+    protected function createTestInstance()
+    {
+        $name = $this->getOneStateNameOfTheCollection();
+        $state = new State($name);
+        return new Process('TestProcess', $state);
+    }
+
+    /**
+     * @see \MetaborStd\Statemachine\StateCollectionInterfaceTest::getOneStateNameOfTheCollection()
+     */
+    protected function getOneStateNameOfTheCollection()
+    {
+        return 'TestState';
+    }
+}
---
+++ tests/Metabor/Statemachine/StateCollectionTest.php
@@ -0,0 +1,32 @@
+<?php
+namespace Metabor\Statemachine;
+use MetaborStd\Statemachine\StateCollectionInterfaceTest;
+
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class StateCollectionTest extends StateCollectionInterfaceTest
+{
+    /**
+     * 
+     * @see \MetaborStd\NamedInterfaceTest::createTestInstance()
+     */
+    protected function createTestInstance()
+    {
+        $name = $this->getOneStateNameOfTheCollection();
+        $state = new State($name);
+        $instance = new StateCollection();
+        $instance->addState($state);
+        return $instance;
+    }
+
+    /**
+     * @see \MetaborStd\Statemachine\StateCollectionInterfaceTest::getOneStateNameOfTheCollection()
+     */
+    protected function getOneStateNameOfTheCollection()
+    {
+        return 'TestState';
+    }
+}
