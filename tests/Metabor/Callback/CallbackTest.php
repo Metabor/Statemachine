17f1d91f14a213e6e4adf6a7898d43d8cec83ae1
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-02 18:33:49 +0200

    add UnitTests

---
+++ tests/Metabor/Callback/CallbackTest.php
@@ -0,0 +1,50 @@
+<?php
+namespace Metabor\Callback;
+
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class CallbackTest extends \PHPUnit_Framework_TestCase
+{
+    private $wasCalled;
+
+    private $parameter;
+
+    public function callableMethod()
+    {
+        $this->wasCalled = true;
+    }
+
+    public function __invoke($parameter)
+    {
+        $this->wasCalled = true;
+        $this->assertSame($this->parameter, $parameter);
+    }
+
+    /**
+     *
+     */
+    public function testConvertsCallableToCallback()
+    {
+        $callable = array($this, 'callableMethod');
+        $callback = new Callback($callable);
+        $this->wasCalled = false;
+        $callback();
+        $this->assertTrue($this->wasCalled);
+    }
+
+    /**
+     *
+     */
+    public function testDeligateAllParamterToTheContainedCallableWhenInvoked()
+    {
+        $callback = new Callback($this);
+        $this->parameter = new \stdClass();
+        $this->wasCalled = false;
+        $callback($this->parameter);
+        $this->assertTrue($this->wasCalled);
+    }
+
+}
---
+++ tests/Metabor/Callback/CompositeTest.php
@@ -0,0 +1,49 @@
+<?php
+namespace Metabor\Callback;
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class CompositeTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param \PHPUnit_Framework_MockObject_Matcher_Invocation $matcher
+     * @param mixed $parameter
+     * @return \MetaborStd\CallbackInterface
+     */
+    protected function createCallbackMock(\PHPUnit_Framework_MockObject_Matcher_Invocation $matcher, $parameter = null)
+    {
+        $mock = $this->getMockForAbstractClass('\MetaborStd\CallbackInterface');
+        $method = $mock->expects($matcher)->method('__invoke');
+        if ($parameter) {
+            $method->with($parameter);
+        } else {
+            $method->withAnyParameters();
+        }
+        return $mock;
+    }
+
+    /**
+     *
+     */
+    public function testCallsAllAttachedCallbacksWhenInvoked()
+    {
+        $composite = new Composite();
+        $parameter = new \stdClass();
+        $callbackAttached1 = $this->createCallbackMock($this->once(), $parameter);
+        $composite->attach($callbackAttached1);
+        $callbackNotAttached1 = $this->createCallbackMock($this->never());
+        $composite->attach($callbackNotAttached1);
+
+        $callbackAttached2 = $this->createCallbackMock($this->once(), $parameter);
+        $composite->attach($callbackAttached2);
+        $callbackNotAttached2 = $this->createCallbackMock($this->never());
+        $composite->attach($callbackNotAttached2);
+
+        $composite->detach($callbackNotAttached1);
+        $composite->detach($callbackNotAttached2);
+
+        $composite($parameter);
+    }
+}
