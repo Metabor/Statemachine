0f1c614cab6d921627f5bbfd5727854151282d57
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-03 23:05:18 +0200

    Add UnitTests

---
+++ tests/Metabor/Statemachine/Transition/ActiveTransitionFilterTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Metabor\Statemachine\Transition;
+
+use MetaborStd\Event\EventInterface;
+
+/**
+ * ActiveTransitionFilter test case.
+ */
+class ActiveTransitionFilterTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @param boolean $active
+     * @param object $subject
+     * @param \ArrayAccess $context
+     * @param EventInterface $event
+     * @return \MetaborStd\Statemachine\TransitionInterface
+     */
+    protected function createTransition($active, $subject, \ArrayAccess $context, EventInterface $event = null)
+    {
+        $transition = $this->getMockForAbstractClass('\MetaborStd\Statemachine\TransitionInterface');
+        $transition->expects($this->atLeastOnce())->method('isActive')->with($subject, $context, $event)
+                ->willReturn($active);
+        return $transition;
+    }
+
+    /**
+     * 
+     */
+    public function testFiltersAllNotActiveTransitions()
+    {
+        $subject = new \stdClass();
+        $context = new \ArrayObject();
+        $event = null;
+
+        $transitions = new \SplObjectStorage();
+        $transitionActive = $this->createTransition(true, $subject, $context, $event);
+        $transitions->attach($transitionActive);
+        $transitionNotActive = $this->createTransition(false, $subject, $context, $event);
+        $transitions->attach($transitionNotActive);
+
+        $filteredTransitions = new ActiveTransitionFilter($transitions, $subject, $context, $event);
+        $this->assertContains($transitionActive, $filteredTransitions);
+        $this->assertNotContains($transitionNotActive, $filteredTransitions);
+    }
+}
