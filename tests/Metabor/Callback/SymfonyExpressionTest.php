a176205466c21b3523d9801e3e1f3d721c023a1a
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-05 22:16:09 +0200

    Add UnitTest

---
+++ tests/Metabor/Callback/SymfonyExpressionTest.php
@@ -0,0 +1,43 @@
+<?php
+namespace Metabor\Callback;
+
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class SymfonyExpressionTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testConvertsSymfonyExpressionToCallback()
+    {
+        $expression = '2 * 3';
+        $callback = new SymfonyExpression($expression);
+        $result = $callback();
+        $this->assertEquals(6, $result);
+    }
+
+    /**
+     *
+     */
+    public function testInvokeParametersAreConsecutivelyMappedToKeys()
+    {
+        $expression = 'a < b';
+        $keys = array('a', 'b');
+        $callback = new SymfonyExpression($expression, $keys);
+        $result = $callback->__invoke(2, 3);
+        $this->assertTrue($result);
+    }
+
+    /**
+     *
+     */
+    public function testExpressionIsUsedAsStringRepresentationOfTheInstance()
+    {
+        $expression = '2 * 3';
+        $callback = new SymfonyExpression($expression);
+        $this->assertEquals($expression, (string) $callback);
+    }
+}
