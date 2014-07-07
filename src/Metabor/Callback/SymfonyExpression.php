3fe62550bad1e892fd03b4cb17fe903716f6268c
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-07 22:19:11 +0200

    Fixes Coding Style

--- src/Metabor/Callback/SymfonyExpression.php
+++ src/Metabor/Callback/SymfonyExpression.php
@@ -61,12 +61,12 @@ class SymfonyExpression extends Expression implements CallbackInterface
      */
     public function __invoke()
     {
-    	if(empty($this->keys)) {
-    		$values = array();
-    	} else {
-    		$args = func_get_args();
-    		$values = array_combine($this->keys, $args);
-    	}
+        if (empty($this->keys)) {
+            $values = array();
+        } else {
+            $args = func_get_args();
+            $values = array_combine($this->keys, $args);
+        }
 
         return $this->expressionLanguage->evaluate($this->getExpression(), $values);
     }
