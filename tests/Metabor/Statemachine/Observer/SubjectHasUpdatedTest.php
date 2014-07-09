f75f72bc5524c2977bbaf86b72d35cead2c354b2
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-10 01:13:14 +0200

    Add UnitTests

---
+++ tests/Metabor/Statemachine/Observer/SubjectHasUpdatedTest.php
@@ -0,0 +1,24 @@
+<?php
+namespace Metabor\Statemachine\Observer;
+
+use Metabor\Observer\Subject;
+
+/**
+ * @author otischlinger
+ *
+ */
+class SubjectHasUpdatedTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * 
+     */
+    public function testCollectAllUpdatedSubjects()
+    {
+        $subject = new Subject();
+        $observer = new SubjectHasUpdated();
+        $subject->attach($observer);
+        $this->assertNotContains($subject, $observer);
+        $subject->notify();
+        $this->assertContains($subject, $observer);
+    }
+}
