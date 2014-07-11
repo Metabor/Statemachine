b61b038d14e684edcfb97ac478774e0e269ca76b
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-07-11 08:25:31 +0200

    Add UnitTest update composer

---
+++ tests/Metabor/Statemachine/Factory/StatefulStateNameDetectorTest.php
@@ -0,0 +1,35 @@
+<?php
+namespace Metabor\Statemachine\Factory;
+
+use Metabor\Statemachine\Process;
+use Metabor\Statemachine\State;
+use MetaborStd\Statemachine\Factory\FactoryInterfaceTest;
+
+/**
+ * 
+ * @author Oliver Tischlinger
+ *
+ */
+class StatefulStateNameDetectorTest extends \PHPUnit_Framework_TestCase
+{
+    public function testReturnsTheCurrentStateFromAStatefulObject()
+    {
+        $name = 'TestStatus';
+        $subject = $this->getMockForAbstractClass('\MetaborStd\Statemachine\StatefulInterface');
+        $subject->expects($this->once())->method('getCurrentStateName')->willReturn($name);
+
+        $detector = new StatefulStateNameDetector();
+        $result = $detector->detectCurrentStateName($subject);
+
+        $this->assertEquals($name, $result);
+    }
+
+    public function testThrowsAnExscptionIfObjectIsNotStateful()
+    {
+        $subject = new \stdClass();
+        $detector = new StatefulStateNameDetector();
+        $this->setExpectedException('\InvalidArgumentException');
+        $detector->detectCurrentStateName($subject);
+    }
+
+}
