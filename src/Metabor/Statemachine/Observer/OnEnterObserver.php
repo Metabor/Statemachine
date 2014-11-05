cc071f45f99c63573a5caa6732419e74245b798e
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2014-11-05 12:18:10 +0100

    add on enter observer

---
+++ src/Metabor/Statemachine/Observer/OnEnterObserver.php
@@ -0,0 +1,38 @@
+<?php
+namespace Metabor\Statemachine\Observer;
+
+use MetaborStd\Statemachine\StatemachineInterface;
+
+/**
+ * Triggers automatically an event if the status changes and the new status has it
+ * 
+ * @author otischlinger
+ *
+ */
+class OnEnterObserver implements \SplObserver
+{
+    /**
+     * @var string
+     */
+    private $eventName;
+
+    /**
+     * @param string $eventName
+     */
+    public function __construct($eventName = 'onEnter')
+    {
+        $this->eventName = $eventName;
+    }
+
+    /**
+     * @see SplObserver::update()
+     */
+    public function update(\SplSubject $stateMachine)
+    {
+        if ($stateMachine instanceof StatemachineInterface) {
+            if ($stateMachine->getCurrentState()->hasEvent($this->eventName)) {
+                $stateMachine->triggerEvent($this->eventName);
+            }
+        }
+    }
+}
