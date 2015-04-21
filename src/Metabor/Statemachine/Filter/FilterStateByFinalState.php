d720154582490d5431f43c3767f51d4ad2c97183
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2015-04-21 17:09:51 +0200

    add final state filter

---
+++ src/Metabor/Statemachine/Filter/FilterStateByFinalState.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace Metabor\Statemachine\Filter;
+
+/**
+ * @author otischlinger
+ */
+class FilterStateByFinalState extends \FilterIterator
+{
+    /**
+     * @see FilterIterator::accept()
+     */
+    public function accept()
+    {
+        /* @var $state \MetaborStd\Statemachine\StateInterface */
+        $state = $this->current();
+
+        return (iterator_count($state->getTransitions()) == 0);
+    }
+}
