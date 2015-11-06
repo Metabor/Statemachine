a0552e08c1e680da55cdba2dd9942da1865b44f5
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2015-11-06 21:41:38 +0100

    add unit test for CounterObserverTest

---
+++ tests/Metabor/Observer/CounterOberserverTest.php
@@ -0,0 +1,37 @@
+<?php
+namespace Metabor\Observer;
+
+/**
+ * @author Oliver Tischlinger
+ */
+class CounterOberserverTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testCountsHowOftenTheObserverWasCalled()
+    {
+        $subject = $this->getMockForAbstractClass('\SplSubject');
+
+        $observer = new CounterObserver();
+        $this->assertEquals(0, $observer->getCount());
+        $observer->update($subject);
+        $observer->update($subject);
+        $this->assertEquals(2, $observer->getCount());
+    }
+
+    /**
+     *
+     */
+    public function testCanBeReset()
+    {
+        $subject = $this->getMockForAbstractClass('\SplSubject');
+
+        $observer = new CounterObserver();
+        $this->assertEquals(0, $observer->getCount());
+        $observer->update($subject);
+        $this->assertEquals(1, $observer->getCount());
+        $observer->resetCount();
+        $this->assertEquals(0, $observer->getCount());
+    }
+}
