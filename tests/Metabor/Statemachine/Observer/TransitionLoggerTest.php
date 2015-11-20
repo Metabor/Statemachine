eb96d808172d72be8251333be11faf48531d78c8
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2015-11-20 21:40:35 +0100

    add unit test for TransitionLogger

---
+++ tests/Metabor/Statemachine/Observer/TransitionLoggerTest.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Metabor\Statemachine\Observer;
+
+use Metabor\Named;
+use Metabor\Statemachine\Process;
+use Metabor\Statemachine\State;
+use Metabor\Statemachine\Statemachine;
+use Psr\Log\LogLevel;
+
+/**
+ * @author otischlinger
+ */
+class TransitionLoggerTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     *
+     */
+    public function testChangesStatusOnStatefulObjects()
+    {
+        $subject = new Named('SubjectName');
+        $state = new State('stateName');
+        $process = new Process('process', $state);
+        $stateMachine = new Statemachine($subject, $process);
+
+        $message = 'Transition for "SubjectName" to "stateName"';
+        $context = array();
+        $context['subject'] = $subject;
+        $context['currentState'] = $state;
+        $context['lastState'] = null;
+        $context['transition'] = null;
+
+        $logger = $this->getMockForAbstractClass('Psr\Log\LoggerInterface');
+        $logger->expects($this->once())->method('log')->with(LogLevel::INFO, $message, $context);
+        $observer = new TransitionLogger($logger);
+        $observer->update($stateMachine);
+    }
+}
