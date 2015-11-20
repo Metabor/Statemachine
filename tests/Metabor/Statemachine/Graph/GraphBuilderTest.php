3ecc774f9cf9800231e6407a05a994b0805152ab
Oliver Tischlinger <oliver.tischlinger@metabor.de>
2015-11-20 22:20:13 +0100

    add Unit Test for GraphBuilder

---
+++ tests/Metabor/Statemachine/Graph/GraphBuilderTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Metabor\Statemachine\Graph;
+
+use Fhaculty\Graph\Graph;
+use Metabor\Statemachine\State;
+use Metabor\Statemachine\StateCollection;
+use Metabor\Statemachine\Transition;
+
+/**
+ * @author Oliver Tischlinger
+ */
+class GraphBuilderTest extends \PHPUnit_Framework_TestCase
+{
+    public function testAddsStatesToGraph()
+    {
+        $state = new State('first');
+        $stateCollection = new StateCollection();
+        $stateCollection->addState($state);
+        $secondState = new State('second');
+        $state->addTransition(new Transition($secondState));
+
+        $graph = new Graph();
+        $builder = new GraphBuilder($graph);
+        $builder->addStateCollection($stateCollection);
+        $this->assertTrue($graph->hasVertex('first'));
+        $this->assertTrue($graph->hasVertex('second'));
+    }
+}
